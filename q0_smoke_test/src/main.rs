c6de1e66339c942e75645d90c8cb6e5af04052ac
Manik Rana <manikrana54@gmail.com>
2023-12-07 11:23:15 +0530

    refactor: log incoming requests

--- q0_smoke_test/src/main.rs
+++ q0_smoke_test/src/main.rs
@@ -1,5 +1,5 @@
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::copy;
+use tokio::io::{copy, AsyncReadExt};
 use std::net::SocketAddr;
 use std::io;
 
@@ -25,6 +25,15 @@ async fn echo(socket: &mut TcpStream, address: SocketAddr) -> io::Result<()> {
 
     let (mut reader, mut writer) = socket.split();
     
+    loop {    
+        let mut buf = vec![0u8; 1024];
+        let bytes = reader.read(&mut buf).await?;
+        if bytes == 0 {
+            break;
+        }
+        let bytes_to_str = String::from_utf8(buf[..bytes].to_vec()).unwrap();
+        println!("{:?}", bytes_to_str);
+    }
     copy(&mut reader, &mut writer).await?;
 
     println!("Closed connection: {}", address);
